471a512c625f4bf36390a965a44fb12e572cefeb
KeruMx <eemmanuel.lunaa@gmail.com>
2020-02-21 09:29:27 -0600

    Splash add

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-void main()=>runApp(Cursos());
+import 'Splash.dart';
+
+void main()=>runApp(Splash());
 
 class Cursos extends StatefulWidget{
   @override
@@ -15,12 +18,13 @@ class CursosForm extends State<Cursos>{
   var isLoading=false;
   List dataCursos;
 
+
   Future<String> gerCursos() async{
     this.setState((){
       isLoading=true;
     });
     var response=await http.get(
-        Uri.encodeFull("http://1.1.1.1:8888/courses"),
+        Uri.encodeFull("http://192.168.43.84:8888/courses"),
         headers: {"Acept": "application/json"}
     );
     this.setState((){
@@ -29,10 +33,20 @@ class CursosForm extends State<Cursos>{
     });
     return "Accept";
   }
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    gerCursos();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      routes: {
+//        '/login'  : (context)=>Login(),
+//        '/dash'   : (context)=>Dashboard(),
+//        '/course' : (context)=>Course()
+      },
       home: Scaffold(
         appBar: AppBar(
           title: Text("Cursos"),
@@ -46,12 +60,44 @@ class CursosForm extends State<Cursos>{
           },
         ),
         body: isLoading ? Center(child: CircularProgressIndicator(),)
-            :ListView.builder(
+            : ListView.builder(
           itemCount: dataCursos==null ? 0 : dataCursos.length,
           itemBuilder: (BuildContext context, int index) {
+            return Slidable(
+              actionPane: SlidableDrawerActionPane(),
+              actionExtentRatio: 0.25,
+              child: Card(
+                elevation: 8.0,
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Color.fromRGBO(121, 181, 237, .9)),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
+                    leading: Container(
+                        padding: EdgeInsets.only(right: 12.0),
+                        decoration: BoxDecoration(
+                            border: Border( right: BorderSide(width: 1.0, color: Colors.black))
+                        ),
+                        child: Icon(Icons.language, color: Colors.black)
+                    ),
 
+                    title: Text(
+                      dataCursos[index]['name'],
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
 
+                    subtitle: Row(
+                      children: <Widget>[
+                        //Icon(Icons.touch_app, color: Colors.yellowAccent),
+                        Text(dataCursos[index]['description'], style: TextStyle(color: Colors.black))
+                      ],
+                    ),
 
+                    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.black, size: 30.0,),
+                  ),
+                ),
+              ),
+            );
           },
         ),
       ),
